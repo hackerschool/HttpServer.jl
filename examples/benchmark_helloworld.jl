1a118277d92d298659f75d40ad7843f4fed81f0e
Jock Lawrie <jock.lawrie@gmail.com>
2015-10-27 11:18:14 +1100

    Added simple benchmark for a helloworld server.
    Signed-off-by: Jock Lawrie <jock.lawrie@gmail.com>

---
+++ examples/benchmark_helloworld.jl
@@ -0,0 +1,23 @@
+# Contents: Basic benchmark
+#
+# To run the benchmark:
+# 1) Select the desired server by commenting out the undesired server
+# 2) Run the server: julia server_benchmark.jl
+# 3) In another terminal:
+#     a) Compile the app:   ab -n 100 -c 1 http://0.0.0.0:8000/
+#     b) Run the benchmark: ab -n 10000 -c 1 http://0.0.0.0:8000/
+
+using HttpServer
+
+function app_new(req::Request)
+    Response("Hello world")
+end
+
+function app_old(req::Request, res::Response)
+    Response("Hello world")
+end
+
+#server = Server(app_old)
+server = Server(app_new)
+
+run(server, 8000)
